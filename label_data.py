50caeddada11d87372013b6a68a2615a8388324a
asharp99 <33327544+asharp99@users.noreply.github.com>
2019-11-14 13:40:43 -0800

    Update label_data.py

--- label_data.py
+++ label_data.py
@@ -2,19 +2,32 @@ import pandas as pd
 
 # Conditions for a report indicating a positive response
 def good_results(results):
-    return "pT0" in results and "pN0" in results
+	if " yp" in results and "T0" in results and "N0" in results:
+		return 1
+	elif " yp" in results:
+		return -1
+    else return 0
 
 # Conditions for a report corresponding to a patient with a future positive response
 def good_results_exist(target_id, target_time, row_id, row_time, response):
     target_day, target_month, target_year = target_time.split('-')
     row_day, row_month, row_year = row_time.split('-')
     later_date = int(row_year) > int(target_year) or (int(row_year) == int(target_year) and (int(row_month) > int(target_month) or (int(row_month) == int(target_month) and int(row_day) > int(target_day))))
-    return target_id == row_id and later_date and response == True
+    return target_id == row_id and later_date and response == 1
+
+# Conditions for a report corresponding to a patient with a future positive response
+def bad_results_exist(target_id, target_time, row_id, row_time, response):
+    target_day, target_month, target_year = target_time.split('-')
+    row_day, row_month, row_year = row_time.split('-')
+    later_date = int(row_year) > int(target_year) or (int(row_year) == int(target_year) and (int(row_month) > int(target_month) or (int(row_month) == int(target_month) and int(row_day) > int(target_day))))
+    return target_id == row_id and later_date and response == -1
 
 # Labels whether patient had positive response at some point in the future
 def label(row_id, row_time, df):
     if True in df.apply(lambda row: good_results_exist(row_id, row_time, row['ANON_ID'], row['RESULT_TIME'], row['GOOD_RESPONSE']), axis = 1).values:
         return 1
+    elif True in df.apply(lambda row: bad_results_exist(row_id, row_time, row['ANON_ID'], row['RESULT_TIME'], row['GOOD_RESPONSE']), axis = 1).values:
+        return -1
     else:
         return 0
 
